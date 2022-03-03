2b3498426680363d5ecd64375c4bad068a2b2e7f
Sonwabile <219267189@mycput.ac.za>
2022-03-03 15:34:09 +0200

    My first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const app = Vue.createApp({
+    data() {
+        return{
+            name: 'Sonwabile',
+            surname: 'Gxoyiya',
+            currentAge: 22,
+            ageplus5: currentAge + 5,
+        }
+    }
+
+
+
+
+
+
+
+})
