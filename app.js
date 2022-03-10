a29da2cd99939eb5034333b263ae717212996d8d
Sonwabile <219267189@mycput.ac.za>
2022-03-10 10:33:15 +0200

    Styling and Vue Outputs.

--- app.js
+++ app.js
@@ -3,28 +3,25 @@ const app = Vue.createApp({
         return{
             name: 'Sonwabile',
             surname: 'Gxoyiya',
+            ageText: 'My Current Age:',
+            ageInFiveYearsText: 'My Age in 5 Years:',
             currentAge: 22,
+            numberGenText: 'Generate Random Number:',
             image: 'joeri-romer-Xne1N4yZuOY-unsplash.jpg',
-            myPhysicalAddress: "1706 FakeAddress Road"
+            addressText: 'My Physical Address:',
+            myPhysicalAddress: "11 Hans Strijdom Ave, Cape Town City Centre"
         };
     },
 
     methods:{
         generateNumber(){
-            //const randNumber = Math.floor(Math.random() * 16);
-            //return randNumber;
             return Math.floor(Math.random() * 16);
         },
 
         ageInFiveYears(){
-            
-            //const ageInFiveYears = this.currentAge + 5;
             return this.currentAge + 5;
         }
-
     }
-
-
 });
 
 app.mount('#profile');
